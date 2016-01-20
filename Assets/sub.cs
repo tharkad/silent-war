094729c5d8fa28c39cfe94c60c75068bf11445aa
Mike Houser <tharkad@casdra.com>
2016-01-19 21:49:01 -0800

    Sub in column icon
    You can now click an icon to show which column the sub is placed.

---
+++ Assets/sub.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sub : MonoBehaviour {
+    public bool on = false;
+    public Sprite[] textures;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            on = (!on);
+            if (on)
+                GetComponent<SpriteRenderer>().sprite = textures[1];
+            else
+                GetComponent<SpriteRenderer>().sprite = textures[0];
+        }
+    }
+}
